80f5e329aa2eb22c8dc87d9c7f18fad545e3a4ee
Ruann <ruann12@gmail.com>
2019-03-11 19:56:55 -0300

    Creating the initial project

---
+++ ShadowverseTracker/ShadowverseTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShadowverseTracker
+//
+//  Created by Ruann Homem on 11/03/19.
+//  Copyright © 2019 Ruann Homem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
